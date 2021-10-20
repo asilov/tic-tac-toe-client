4cfc2cb68ecf9e0ee37bbd992ee3bbb9ce62ff3a
asilov <asil.california@gmail.com>
2021-10-20 14:06:20 -0700

    Update

--- app/app.js
+++ app/app.js
@@ -5,9 +5,22 @@ const authEvents = require('./auth/events')
 // require('./example')
 
 $(() => {
-  // your JS code goes here
-  $("#signUp").on("submit", authEvents.onSignUp)
-  $("#signIn").on("submit", authEvents.onSignIn)
-  $("#changePassword").on("submit", authEvents.onChangePassword)
-  $("#signOut").on("click", authEvents.onSignOut)
+
+  $("#sign-up").on("submit", authEvents.onSignUp);
+  $("#sign-in").on("submit", authEvents.onSignIn);
+  $("#sign-out").on("submit", authEvents.onSignOut);
+  $("#new-game").on("submit", authEvents.onNewGame);
+  $("#resetGame").on("submit", authEvents.onNewGameReset);
+
+
+  
+  $(".play1").on("click", authEvents.onClick);
+  $(".play2").on("click", authEvents.onClick);
+  $(".play3").on("click", authEvents.onClick);
+  $(".play4").on("click", authEvents.onClick);
+  $(".play5").on("click", authEvents.onClick);
+  $(".play6").on("click", authEvents.onClick);
+  $(".play7").on("click", authEvents.onClick);
+  $(".play8").on("click", authEvents.onClick);
+  $(".play9").on("click", authEvents.onClick);
 })
